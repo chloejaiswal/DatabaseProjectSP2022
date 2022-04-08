f74bbfa0df37723424f437847d20e73267459e38
Chloe Jaiswal <chloe.jaiswal@yahoo.com>
2022-04-07 21:48:18 -0400

    Add files via upload
    DML script to add data into team table

---
+++ TeamDataDML.sql
@@ -0,0 +1,46 @@
+/* DML Script to Load the data in to the Team Table
+*/
+
+USE Soccer
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(1, 'Angle City FC', 'Los Angeles', 'CA', 'Freya Coombe');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(2, 'San Diego Wave FC', 'San Diego', 'CA', 'Casey Stoney');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(3, 'Chicago Red Stars', 'Chicago', 'IL', 'Chris Petrucelli');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(4, 'Orlando Pride', 'Orlando', 'FL', 'Amanda Cromwell');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(5, 'OL Reign', 'Tacoma', 'WA', 'Laura Harvey');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(6, 'FC Cincinnati', 'Cincinnati', 'OH', 'Chris Albright');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(7, 'Columbus Crew', 'Columbus', 'OH', 'Caleb Porter');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(8, 'Minnesota United', 'Saint Paul', 'MN', 'Adrian Heath');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(9, 'Orlando City', 'Orlando', 'FL', 'Oscar Pareja');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(10, 'New York Red Bulls', 'Harrison', 'NJ', 'Gerhard Struber');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(11, 'Portland Thorns FC', 'Portlond', 'OR', 'Rhian Emilie Wilkinson');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(12, 'Racing Louisville FC', 'Louisville', 'KY', 'Lars Kim Bjoerkegren');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(13, 'Houston Dash', 'Houston', 'TX', 'James Clarkson');
+
+INSERT INTO TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(14, 'Kansas City Current', 'Kansas City', 'KS', 'Matthew James Potter');
