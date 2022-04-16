3408abd6a6e373561c4dd3b5bb43b0207c6e64f2
Chloe Jaiswal <chloe.jaiswal@yahoo.com>
2022-04-16 02:02:33 -0400

    Task 11: Create a non-default schema

---
+++ NondefaultSchema/NondefaultSchema.sql
@@ -0,0 +1,6 @@
+/* Chloe Jaiswal
+4/16/2022
+Creates nondefault schema
+*/
+CREATE SCHEMA Soccer_League;
+GO
---
+++ NondefaultSchema/TeamDataNondefaultSchemaDML.sql
@@ -0,0 +1,49 @@
+/* Chloe Jaiswal
+4/16/2022 
+DML Script to Load the data in to the Team Table nondefault schema
+*/
+
+USE Soccer
+GO
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(1, 'Angle City FC', 'Los Angeles', 'CA', 'Freya Coombe');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(2, 'San Diego Wave FC', 'San Diego', 'CA', 'Casey Stoney');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(3, 'Chicago Red Stars', 'Chicago', 'IL', 'Chris Petrucelli');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(4, 'Orlando Pride', 'Orlando', 'FL', 'Amanda Cromwell');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(5, 'OL Reign', 'Tacoma', 'WA', 'Laura Harvey');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(6, 'FC Cincinnati', 'Cincinnati', 'OH', 'Chris Albright');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(7, 'Columbus Crew', 'Columbus', 'OH', 'Caleb Porter');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(8, 'Minnesota United', 'Saint Paul', 'MN', 'Adrian Heath');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(9, 'Orlando City', 'Orlando', 'FL', 'Oscar Pareja');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(10, 'New York Red Bulls', 'Harrison', 'NJ', 'Gerhard Struber');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(11, 'Portland Thorns FC', 'Portlond', 'OR', 'Rhian Emilie Wilkinson');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(12, 'Racing Louisville FC', 'Louisville', 'KY', 'Lars Kim Bjoerkegren');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(13, 'Houston Dash', 'Houston', 'TX', 'James Clarkson');
+
+INSERT INTO Soccer_League.TEAM(TEAM_ID, TEAM_NAME, TEAM_CITY, TEAM_STATE, TEAM_HEAD_COACH)
+VALUES(14, 'Kansas City Current', 'Kansas City', 'KS', 'Matthew James Potter');
